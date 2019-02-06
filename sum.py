23bf9284cba91cec83477d4539aca8d0323600f1
mercymap <mercymckarl@gmail.com>
2019-02-06 15:40:17 +0300

    add sum.py

--- sum.py
+++ sum.py
@@ -1,5 +1,14 @@
+
 mylist = [1, 2, [3, 4]]
-x = [1, 2]
-y = [3, 4]
-z = x + y
-print(sum(z))
+
+
+def find_total_sum(numbers):
+    total = 0
+
+    for index in len(numbers):
+        if isinstance (numbers[index], list):
+            total += find_total_sum(numbers[index])
+        total += numbers[index]
+    return total
+
+print(find_total_sum)
